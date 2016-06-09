b24df8358e8fe2263b9cc95df998ead7e9c5ed4d
Dana Centeno <danacentenodes@gmail.com>
2016-06-09 00:06:15 -0400

    changed styling of header and about sections

--- front-page.php
+++ front-page.php
@@ -7,7 +7,14 @@
 get_header(); ?>
 
 <div class="hero"> <!-- .hero header starts -->
-	
+   <div class="hero-text"> <!-- .hero-text starts -->
+      <h1>
+          <?php bloginfo( 'name' ); ?>
+      </h1>
+      <h2>designer & developer</h2>
+    </div> <!-- .hero-text ends -->
+    
+	<img src="http://unsplash.it/600/600?image=1053" class="accent-img" alt="">
 </div> <!-- .hero header ends -->
 
 <div class="main"> <!-- main content starts -->
@@ -15,27 +22,34 @@ get_header(); ?>
     <?php // Start the loop ?>
     <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-      <section class="about"> <!-- start .about section -->
+      <section id="bio" class="about"> <!-- start .about section -->
         <div class="wrapper"> <!-- .wrapper starts -->
-          <h2><?php //the_title(); ?></h2>
           <?php $image = get_field('bio_image'); ?> <!-- bio image custom field 1/2-->
-          <img class="bio-image" src="<?php echo $image['sizes']['medium'] ?>"> <!-- bio image custom field 2/2-->
-          <p class="bio-blurb"><?php the_field('bio'); ?></p> <!-- bio blurb custom field -->
+          <img class="bio-image" src="<?php echo $image['sizes']['large'] ?>"> <!-- bio image custom field 2/2-->
+          <div class="bio-copy">
+            <h3>About</h3>
+            <p class="bio-blurb"><?php the_field('bio'); ?></p> <!-- bio blurb custom field -->
+          </div>
 
-          <?php the_content(); ?>
         </div> <!-- .wrapper ends -->
       </section> <!-- end .about section -->
 
     <?php endwhile; // end the loop?>
 
-    <section class="skillset"> <!-- .skillset section starts -->
+    <section id="dem-skillz" class="skillset"> <!-- .skillset section starts -->
       <div class="wrapper">
-        <div class="skills"></div>
-        <div class="tools"></div>
+        <div class="skills-copy">
+          <h4>Things I know</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat sint maxime voluptates at, culpa ex saepe nulla voluptatibus incidunt veniam!</p>
+        </div>
+
+        <div class="skills-icons"></div>
       </div>
     </section> <!-- .skillset section ends -->
 
-    <!-- Portfolio section would be in between skills and contact, on separate php file -->
+    <section class="portfolio">
+      
+    </section>
 
     <section class="contact"> <!-- .contact section starts -->
       
